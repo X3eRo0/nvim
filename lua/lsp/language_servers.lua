db628d8d55492846c0dd49ae89b553db9fc9b15d
X3eRo0 <psinghania256@gmail.com>
2022-03-23 03:44:41 +0530

    asdf

--- lua/lsp/language_servers.lua
+++ lua/lsp/language_servers.lua
@@ -1,153 +1,23 @@
--- Setup lspconfig.
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local lsp_installer = require('nvim-lsp-installer')
 
-local system_name
-if vim.fn.has("mac") == 1 then
-    system_name = "macOS"
-elseif vim.fn.has("unix") == 1 then
-    system_name = "Linux"
-elseif vim.fn.has("win32") == 1 then
-    system_name = "Windows"
-else
-    print("Unsupported system for sumneko")
-end
-
--- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
-local sumneko_root_path = "/home/x3ero0/.config/nvim/lua-language-server"
-local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name .. "/lua-language-server"
-
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
-local langservers = {
-    "html",
-    "cssls",
-    "pylsp",
-    "sumneko_lua",
-    "clangd",
-    "cmake"
-}
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
-    local function buf_set_option(...)
-        vim.api.nvim_buf_set_option(bufnr, ...)
-    end
-
-    -- Enable completion triggered by <c-x><c-o>
-    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
-    -- Mappings.
-    local opts = {noremap = true, silent = true}
-
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    buf_set_keymap("n", "gdp", "<cmd>Lspsaga preview_definition<CR>", opts)
-    buf_set_keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
-    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    buf_set_keymap("n", "gk", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-    buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    buf_set_keymap("n", "<space>rn", "<cmd>Lspsaga rename<CR>", opts)
-    buf_set_keymap("n", "<space>ca", "<cmd>Lspsaga code_action<CR>", opts)
-    buf_set_keymap("v", "<space>ca", ":<C-U>Lspsaga range_code_action<CR>", opts)
-    -- buf_set_keymap("n", "<space>ca", "<cmd>CodeActionMenu<CR>", opts)
-    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-    buf_set_keymap("n", "<space>de", "<cmd>Lspsaga show_line_diagnostics<cr>", opts)
-    buf_set_keymap("n", "<C-f>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", opts)
-    buf_set_keymap("n", "<C-b>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", opts)
-    -- buf_set_keymap(
-    --     "n",
-    --     "<space>f",
-    --     "<cmd>lua vim.lsp.buf.formatting({tabSize=4, insertSpaces=true, trimTrailingWhitespace=true})<CR>",
-    --     opts
-    -- )
-    -- buf_set_keymap(
-    --     "n",
-    --     "<leader>de",
-    --     "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = border })<CR>",
-    --     opts
-    -- )
-    -- buf_set_keymap("n", "[d", '<Cmd>lua vim.lsp.diagnostic.goto_prev({popup_opts = {source = "always"}})<CR>', opts)
-    -- buf_set_keymap("n", "]d", '<Cmd>lua vim.lsp.diagnostic.goto_next({popup_opts = {source = "always"}})<CR>', opts)
-end
-
-for _, server in ipairs(langservers) do
-    if server == "sumneko_lua" then
-        require "lspconfig"[server].setup {
-            cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-            on_attach = on_attach,
-            settings = {
-                Lua = {
-                    runtime = {
-                        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                        version = "LuaJIT",
-                        -- Setup your lua path
-                        path = runtime_path
-                    },
-                    diagnostics = {
-                        -- Get the language server to recognize the `vim` global
-                        globals = {"vim"}
-                    },
-                    workspace = {
-                        -- Make the server aware of Neovim runtime files
-                        library = vim.api.nvim_get_runtime_file("", true),
-                        checkThirdParty = false
-                    },
-                    -- Do not send telemetry data containing a randomized but unique identifier
-                    telemetry = {
-                        enable = false
-                    }
-                }
-            }
+lsp_installer.on_server_ready(function(server)
+  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol
+                                                                       .make_client_capabilities())
+  local opts = {capabilities = capabilities}
+  if server.name == "sumneko_lua" then
+    opts = vim.tbl_deep_extend("force", {
+      settings = {
+        Lua = {
+          runtime = {version = 'LuaJIT', path = vim.split(package.path, ';')},
+          diagnostics = {globals = {'vim'}},
+          workspace = {library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false},
+          telemetry = {enable = false}
         }
-    else
-        require "lspconfig"[server].setup {
-            on_attach = on_attach,
-            capabilities = capabilities
-        }
-    end
-end
+      }
 
-local function goto_definition(split_cmd)
-    local util = vim.lsp.util
-    local log = require("vim.lsp.log")
-    local api = vim.api
+    }, opts)
+  end
+  server:setup(opts)
+end)
 
-    -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
-    local handler = function(_, result, ctx)
-        if result == nil or vim.tbl_isempty(result) then
-            local _ = log.info() and log.info(ctx.method, "No location found")
-            return nil
-        end
-
-        if split_cmd then
-            vim.cmd(split_cmd)
-        end
-
-        if vim.tbl_islist(result) then
-            util.jump_to_location(result[1])
-
-            if #result > 1 then
-                util.set_qflist(util.locations_to_items(result))
-                api.nvim_command("copen")
-                api.nvim_command("wincmd p")
-            end
-        else
-            util.jump_to_location(result)
-        end
-    end
-
-    return handler
-end
-
-vim.lsp.handlers["textDocument/definition"] = goto_definition("vsplit")
+-- vim.lsp.handlers["textDocument/definition"] = goto_definition("vsplit")
