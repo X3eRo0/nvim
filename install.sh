6d0e232aca3b8ef864dc0a755cda6b6f27d47f58
X3eRo0 <psinghania256@gmail.com>
2022-03-26 21:10:08 +0530

    asdf

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
-sudo apt install xclip
+sudo apt install -y xclip clang-format
+npm install --save-dev --save-exact prettier
+npm install --save-dev --save-exact lua-fmt
+curl -sS https://webinstall.dev/shfmt | bash
