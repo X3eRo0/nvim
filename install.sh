0c741bdaeb329da28db35994e3ba49f06eb64bdf
X3eRo0 <psinghania256@gmail.com>
2022-03-26 21:14:06 +0530

    coasdf

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ sudo apt install -y xclip clang-format
 npm install --save-dev --save-exact prettier
 npm install --save-dev --save-exact lua-fmt
 curl -sS https://webinstall.dev/shfmt | bash
+python -m pip install black
