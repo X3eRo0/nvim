61d5798790b54f77254d775b8c4cc624f6c6dce7
X3eRo0 <psinghania256@gmail.com>
2023-12-22 18:11:27 -0700

    Fixed.

--- lua/compile-config/init.lua
+++
@@ -1,6 +0,0 @@
-require("compile-mode").setup({
-    -- you can disable colors by uncommenting this line
-    -- no_baleia_support = true,
-    default_command = "",
-    recompile_no_fail = true,
-})
