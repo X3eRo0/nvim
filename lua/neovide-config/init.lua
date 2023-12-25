04f242c585f5ab8e57502448757acb5ec39e2609
X3eRo0 <psinghania256@gmail.com>
2023-12-25 14:36:15 -0700

    Fixed neovide cursor animation length.

--- lua/neovide-config/init.lua
+++ lua/neovide-config/init.lua
@@ -11,7 +11,7 @@ if vim.g.neovide then
     vim.g.neovide_remember_window_size = true
 
     -- cursor settings.
-    vim.g.neovide_cursor_animation_length = 0.2
+    vim.g.neovide_cursor_animation_length = 0.1
     vim.g.neovide_cursor_trail_size = 0
     vim.g.neovide_cursor_animate_in_insert_mode = false
     vim.g.neovide_cursor_animate_command_line = false
